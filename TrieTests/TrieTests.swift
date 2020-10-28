0811c1be1e03325e7b6ffb29084807b592bf74b9
Frank McAuley <frank.mcauley@gmail.com>
2020-10-27 20:59:10 -0400

    FM - Initial commit

--- TrieTests/TrieTests.swift
+++ TrieTests/TrieTests.swift
@@ -18,16 +18,8 @@ class TrieTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testThatTrieIsNotNil() throws {
+        let trie = Trie()
+        XCTAssertNotNil(trie)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
