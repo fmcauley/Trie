451decb4affb7b38ef632e032b139d33dc1a27e1
Frank McAuley <frank.mcauley@gmail.com>
2020-10-27 20:58:29 -0400

    FM - Adding the git ignore file to the project

---
+++ Trie/Trie.swift
@@ -0,0 +1,8 @@
+//
+//  Trie.swift
+//  Trie
+//
+//  Created by Frank McAuley on 10/27/20.
+//
+
+import Foundation
---
+++ Trie/TrieNode.swift
@@ -0,0 +1,8 @@
+//
+//  TrieNode.swift
+//  Trie
+//
+//  Created by Frank McAuley on 10/27/20.
+//
+
+import Foundation
