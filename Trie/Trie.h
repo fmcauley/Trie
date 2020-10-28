d385a0895aaa05a0a56cd677dd8f707e668caa72
Frank McAuley <frank.mcauley@gmail.com>
2020-10-27 20:51:55 -0400

    Initial Commit

---
+++ Trie/Trie.h
@@ -0,0 +1,18 @@
+//
+//  Trie.h
+//  Trie
+//
+//  Created by Frank McAuley on 10/27/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Trie.
+FOUNDATION_EXPORT double TrieVersionNumber;
+
+//! Project version string for Trie.
+FOUNDATION_EXPORT const unsigned char TrieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Trie/PublicHeader.h>
+
+
